ef95d64fe257b10c98d387956fc98c3b26b3e316
retereum <pavelnikolaichev@gmail.com>
2024-08-23 10:34:43 +0800

    Small changes for Transformers, added quanto for quantization

--- LLM/TransformersLLM.py
+++ LLM/TransformersLLM.py
@@ -1,21 +1,31 @@
 import sys
 import torch
-from transformers import pipeline, AutoModelForCausalLM, AutoTokenizer
+from transformers import (
+    pipeline,
+    AutoModelForCausalLM,
+    AutoTokenizer,
+    QuantoConfig,
+)
+
+device = "cpu"
 
 
-# TODO: set appropriate model
-def get_transformers_pipeline(model_name: str = "facebook/opt-350m") -> pipeline:
+def get_transformers_pipeline(model_name: str = "Qwen/Qwen2-0.5B") -> pipeline:
     """
     Function to get a Transformers pipeline that can be used to generate output.
 
     Args:
-        model_name (str): The name of the model to load. Defaults to "facebook/opt-350m".
+        model_name (str): The name of the model to load. Defaults to "Qwen/Qwen2-0.5B".
 
     Returns:
         pipeline: The Transformers pipeline instance.
     """
+    quantization_config = QuantoConfig(weights="int2")
+
     tokenizer = AutoTokenizer.from_pretrained(model_name)
-    model = AutoModelForCausalLM.from_pretrained(model_name, device_map="auto")
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name, device_map="cpu", quantization_config=quantization_config
+    )
 
     # Compilation doesn't work with Python 3.12+ yet
     if sys.version_info < (3, 12):
@@ -23,10 +33,15 @@ def get_transformers_pipeline(model_name: str = "facebook/opt-350m") -> pipeline
             model.forward, mode="reduce-overhead", fullgraph=True
         )
 
-    return pipeline("text-generation", model=model, tokenizer=tokenizer)
+    return pipeline(
+        "text-generation",
+        model=model,
+        tokenizer=tokenizer,
+        device_map="auto",
+    )
 
 
-def generate_transformers_output(prompt: str, pipeline=None) -> str:
+def generate_transformers_output(prompt: str, pipeline: pipeline = None) -> str:
     """
     Function to generate an output from the Transformers pipeline.
 
@@ -40,6 +55,16 @@ def generate_transformers_output(prompt: str, pipeline=None) -> str:
     if pipeline is None:
         pipeline = get_transformers_pipeline()
 
-    response = pipeline(prompt, max_length=100, do_sample=True)
+    messages = [
+        {
+            "role": "system",
+            "content": "You are a helpful assistant in a university environment. Help professors and students with their questions and problems.",
+        },
+        {"role": "user", "content": prompt},
+    ]
 
-    return response[0]["generated_text"]
+    response = pipeline(messages, max_new_tokens=100, do_sample=True)
+
+    print(response)
+
+    return response[0]["generated_text"][-1]
