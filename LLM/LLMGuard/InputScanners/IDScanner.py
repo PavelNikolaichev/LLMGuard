63ce80074f810e7302ca97df82055bd91749a593
retereum <pavelnikolaichev@gmail.com>
2024-08-23 10:36:34 +0800

    Added different masked tag for each scanner.

--- LLM/LLMGuard/InputScanners/IDScanner.py
+++ LLM/LLMGuard/InputScanners/IDScanner.py
@@ -1,4 +1,9 @@
 from llm_guard.input_scanners import Regex
+from presidio_anonymizer.core.text_replace_builder import TextReplaceBuilder
+
+from llm_guard.util import get_logger
+
+LOGGER = get_logger()
 
 
 class IDScanner(Regex):
@@ -7,7 +12,36 @@ class IDScanner(Regex):
     """
 
     def __init__(self, *args, **kwargs):
-        super().__init__([r'\b([A-Z]{2,4})?\d{6,10}\b'], *args, **kwargs)
+        super().__init__([r"([A-Z]{2,4})?\d{6,10}"], *args, **kwargs)
 
     def get_name(self):
         return "IDScanner"
+
+    def scan(self, prompt: str) -> tuple[str, bool, float]:
+        text_replace_builder = TextReplaceBuilder(original_text=prompt)
+        for pattern in self._patterns:
+            match = self._match_type.match(pattern, prompt)
+            if match is None:
+                continue
+
+            if self._is_blocked:
+                LOGGER.warning("Pattern was detected in the text", pattern=pattern)
+
+                if self._redact:
+                    text_replace_builder.replace_text_get_insertion_index(
+                        "[ID OMMITED]",
+                        match.start(),
+                        match.end(),
+                    )
+
+                return text_replace_builder.output_text, False, 1.0
+
+            LOGGER.debug("Pattern matched the text", pattern=pattern)
+            return text_replace_builder.output_text, True, 0.0
+
+        if self._is_blocked:
+            LOGGER.debug("None of the patterns were found in the text")
+            return text_replace_builder.output_text, True, 0.0
+
+        LOGGER.warning("None of the patterns matched the text")
+        return text_replace_builder.output_text, False, 1.0
--- LLM/LLMGuard/InputScanners/NNumberScanner.py
+++ LLM/LLMGuard/InputScanners/NNumberScanner.py
@@ -1,14 +1,50 @@
 from llm_guard.input_scanners import Regex
+from presidio_anonymizer.core.text_replace_builder import TextReplaceBuilder
+
+from llm_guard.util import get_logger
+
+LOGGER = get_logger()
 
 
 class NNumberScanner(Regex):
     """
     Scanner for N numbers
-    N number is a university id number that starts with letter N and is followed by 8 digits
+
+    According to NYU info for newcoming students,
+    N number is a university id number that starts with letter N and is followed by 8 digits.
     """
 
     def __init__(self, *args, **kwargs):
-        super().__init__([r"\bN\d{8}\b"], *args, **kwargs)
+        super().__init__([r"N\d{8}"], *args, **kwargs)
 
     def get_name(self):
         return "NNumberScanner"
+
+    def scan(self, prompt: str) -> tuple[str, bool, float]:
+        text_replace_builder = TextReplaceBuilder(original_text=prompt)
+        for pattern in self._patterns:
+            match = self._match_type.match(pattern, prompt)
+            if match is None:
+                continue
+
+            if self._is_blocked:
+                LOGGER.warning("Pattern was detected in the text", pattern=pattern)
+
+                if self._redact:
+                    text_replace_builder.replace_text_get_insertion_index(
+                        "[StudentID OMMITED]",
+                        match.start(),
+                        match.end(),
+                    )
+
+                return text_replace_builder.output_text, False, 1.0
+
+            LOGGER.debug("Pattern matched the text", pattern=pattern)
+            return text_replace_builder.output_text, True, 0.0
+
+        if self._is_blocked:
+            LOGGER.debug("None of the patterns were found in the text")
+            return text_replace_builder.output_text, True, 0.0
+
+        LOGGER.warning("None of the patterns matched the text")
+        return text_replace_builder.output_text, False, 1.0
